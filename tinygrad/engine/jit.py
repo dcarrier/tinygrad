ae1db052300feb6066c9bbd8bb8a1612362b280f
dcarrier <carrierdarius@gmail.com>
2024-08-24 13:45:53 -0700

    buffer_replace and jit_cache need to be marked deletable

--- tinygrad/engine/jit.py
+++ tinygrad/engine/jit.py
@@ -183,6 +183,8 @@ def _prepare_jit_inputs(args, kwargs):
   return input_buffers, var_vals, names, st_vars_dtype_device
 
 class TinyJit(Generic[ReturnType]):
+  __deletable__ = ["_buffer_replace", "_jit_cache"]
+
   def __init__(self, fxn:Optional[Callable[..., ReturnType]], captured:Optional[CapturedJit]=None):
     assert fxn or captured, "need either a function or a CapturedJit"
     self.fxn = fxn
